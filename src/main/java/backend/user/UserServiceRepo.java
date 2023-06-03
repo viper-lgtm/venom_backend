8d60e68b37b38940f70b1d95390a7bba1b2c87e9
martinve <martinvetsu@gmail.com>
2023-06-03 09:24:23 +0200

    testing default user

--- src/main/java/backend/user/UserServiceRepo.java
+++ src/main/java/backend/user/UserServiceRepo.java
@@ -13,11 +13,12 @@ public class UserServiceRepo implements UserService {
 
     @Override
     public User create(User user) {
-        Optional<User> userByEmail = repository.findByEmail(user.getEmail());
-        if (!userByEmail.isPresent()) {
-            return repository.save(user);
-        }
-        return null;
+//        Optional<User> userByEmail = repository.findByEmail(user.getEmail());
+//        if (!userByEmail.isPresent()) {
+//            return repository.save(user);
+//        }
+//        return null;
+        return repository.save(user);
     }
 
     @Override
